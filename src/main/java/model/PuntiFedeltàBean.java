ef44ee43938a6bc8fa07fba6a1b163ef46a903ef
Giusy03 <g.chierchia7@studenti.unisa.it>
2024-05-20 14:45:00 +0200

    First Commit

---
+++ src/main/java/model/PuntiFedeltàBean.java
@@ -0,0 +1,33 @@
+package model;
+
+public class PuntiFedeltàBean {
+ private int id_punti;
+ private int id_utente;
+private int punti_accumulati;
+ private int dataaggiornamento;
+public int getId_punti() {
+	return id_punti;
+}
+public void setId_punti(int id_punti) {
+	this.id_punti = id_punti;
+}
+public int getId_utente() {
+	return id_utente;
+}
+public void setId_utente(int id_utente) {
+	this.id_utente = id_utente;
+}
+public int getPunti_accumulati() {
+	return punti_accumulati;
+}
+public void setPunti_accumulati(int punti_accumulati) {
+	this.punti_accumulati = punti_accumulati;
+}
+public int getDataaggiornamento() {
+	return dataaggiornamento;
+}
+public void setDataaggiornamento(int dataaggiornamento) {
+	this.dataaggiornamento = dataaggiornamento;
+}
+ 
+}
